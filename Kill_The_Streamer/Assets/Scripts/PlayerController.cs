a9f95d0fb78715444d77549b233c8fe4b1ff2cd8
Daniel Lowery (RIT Student) <dtl5161@ad.rit.edu>
2017-02-07 13:22:28 -0500

    push

--- Kill_The_Streamer/Assets/Scripts/PlayerController.cs
+++ Kill_The_Streamer/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    //variables
-    float speed = 1.0f;
-    Rigidbody2D player;
-
 	// Use this for initialization
 	void Start () {
 		
@@ -15,25 +11,6 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //check for input
-        if (Input.GetKey(KeyCode.W))
-        {
-            player.AddForce(Vector2.up * speed);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.AddForce(Vector2.left * speed);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            player.AddForce(Vector2.down * speed);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            player.AddForce(Vector2.right * speed);
-        }
-    }
+		
+	}
 }
