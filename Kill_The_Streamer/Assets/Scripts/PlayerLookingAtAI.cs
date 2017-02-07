9f4c38c8561d6352aaecb3dd433dbb0a43319d62
Ro <rc7604@g.rit.edu>
2017-02-07 13:24:01 -0500

    Working on field of view for player to AI.

--- Kill_The_Streamer/Assets/Scripts/PlayerLookingAtAI.cs
+++ Kill_The_Streamer/Assets/Scripts/PlayerLookingAtAI.cs
@@ -8,7 +8,7 @@ public class PlayerLookingAtAI : MonoBehaviour {
     // Use this for initialization
     private bool targetAi;//tells if the player is currently looking at the AI that is supposed to avoid the player if he/she is looking at it
 
-    //public float 
+    public float rangeMult = 5.0f;//multiplying the field of view for the player. 
 
 	void Start () {
         targetAi = false;
@@ -33,8 +33,29 @@ public class PlayerLookingAtAI : MonoBehaviour {
     {
         RaycastHit hit;
         //getting the forward vector of the player
-        Vector3 forwardsPlayer = transform.forward;
+        Vector3 leftAngle = Quaternion.AngleAxis(-45, transform.up) * transform.forward;
+        Vector3 rightAngle = Quaternion.AngleAxis(45, transform.up) * transform.forward;
+        Debug.DrawLine(transform.position + rightAngle, transform.position);
+        Debug.DrawLine(transform.position + leftAngle, transform.position);
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("BooAi");
+
+        for(int i = 0; i < enemies.Length; ++i)
+        {
+            Vector3 playerToEnemy = enemies[0].transform.position - transform.position;
+            bool positiveLeft = (Vector3.Dot(leftAngle, playerToEnemy) >= 0);
+            bool positiveRight = (Vector3.Dot(rightAngle, playerToEnemy) >= 0);
+            if(positiveLeft && positiveRight)
+            {
+                //runaway
+
+            }
+
+        }
+
+
         //test to see if the player is facing an object
+        /*
         if (Physics.Raycast(transform.position, forwardsPlayer, out hit, 20.0f ))
         {
             //if the rayCast hits the Ai that needs to test it is hit, then make sure it is fine
@@ -47,5 +68,6 @@ public class PlayerLookingAtAI : MonoBehaviour {
                 targetAi = false;
             }
         }
+        */
     }
 }
