9ab63cb0b056c859984ee51626d955c79d19febe
MAIN\rc7604 <rc7604@g.rit.edu>
2017-02-10 12:55:32 -0500

    Working on ghost script. Have state enum set up. Currently implamenting.

--- Kill_The_Streamer/Assets/Scripts/AiGhost.cs
+++ Kill_The_Streamer/Assets/Scripts/AiGhost.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+    public enum ghostStates
+    {
+        slowSeek,
+        chargeSeek,
+        standStill
+    }
 public class AiGhost : MonoBehaviour {
 
     // Use this for initialization
 
+
     public float startSpeed =2.0f;//the starting slow speed for the ghost
     public float chargeSpeed=9.0f;//the charging fast speed for the ghost once player is spotted
+    public float rotationSpeed = 10.0f;//how fast the ghost will rotate
 
     public float pauseTimer=2000.0f;//the amount of time the ghost will pause for
     private float currTimer=0.0f;//the current timer the ghost
@@ -16,6 +25,8 @@ public class AiGhost : MonoBehaviour {
 
     private Vector3 chargeLoc;//the location the ghost will charge to once timer is over
 
+    private ghostStates ghostMovement;
+    private bool toCharge;
 
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -26,9 +37,20 @@ public class AiGhost : MonoBehaviour {
 		
 	}
 
-    public void Seeking(Vector3 p_goTo)
+   /// <summary>
+   /// Moving the ghost to a location of the player(will change based on what state it is in.
+   /// </summary>
+   /// <param name="p_goTo">The position that the ghost will go to</param>
+   /// <param name="p_speed">The speed that the ghost will go to that position at</param>
+    public void Seeking(Vector3 p_goTo, float p_speed)
     {
+        Vector3 ghostPosition = transform.position;
+        //rotating the ghost
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(p_goTo - transform.position), rotationSpeed * Time.deltaTime);
 
+        //moving the ghost
+        ghostPosition += this.transform.forward * p_speed * Time.deltaTime;
+        transform.position = ghostPosition;
     }
 
 
