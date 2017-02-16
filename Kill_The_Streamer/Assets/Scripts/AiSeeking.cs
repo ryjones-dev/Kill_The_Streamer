0e06a906c2b35960777d3d2f88f3a56158c30c31
MAIN\rc7604 <rc7604@g.rit.edu>
2017-02-16 13:49:41 -0500

    Shield AI enemy will seek out slowly the player and will potentially block damage. Working on possible leader following for the seeking enemies so they get behind shield.

--- Kill_The_Streamer/Assets/Scripts/AiSeeking.cs
+++ Kill_The_Streamer/Assets/Scripts/AiSeeking.cs
@@ -9,6 +9,8 @@ public class AiSeeking : MonoBehaviour {
     //all AI needs using UnityEngine.AI;
     private GameObject player;//the target to seek (player)
     private NavMeshAgent nav;//the navmeshAgent for the current AI. All AIs need a navMeshAgent to work.
+    private ShieldAi shield;//the air to detect if the shield is up to follow
+    private GameObject shieldGameObject;//the gameobject of the shield in order to detect which one is closest
 
 	void Start () {
         //finding object with the tag "Player"
@@ -23,6 +25,7 @@ public class AiSeeking : MonoBehaviour {
         // Based on NavMesh.
         // Changing speed and acceleration can be found in inspector.
         nav.SetDestination(player.transform.position);//telling the AI to seek out and go to the player's location
+        ClosestShield();
 
     }
 
@@ -34,5 +37,35 @@ public class AiSeeking : MonoBehaviour {
     public void Seek()
     {
         nav.SetDestination(player.transform.position);//telling the AI to seek out and go to the player's location
+      
+    }
+
+    /// <summary>
+    /// This is for checking and finding the closest shield and if so then see it and follow until close enough to player
+    /// </summary>
+    public void ClosestShield()
+    {
+        //check to make sure there is atleast one shield in the scene
+        if(GameObject.FindGameObjectsWithTag("Shield")!=null)
+        {
+            GameObject[] shieldsInLevel = GameObject.FindGameObjectsWithTag("Shield");
+            float closeDist = 1000.0f;//setting it to some arbitrarily high num to then change
+
+            foreach(GameObject closeObject in shieldsInLevel)
+            {
+                float dist = Vector3.Distance(this.transform.position, closeObject.transform.position);
+
+                if(dist < closeDist)
+                {
+                    closeDist = dist;
+                    shieldGameObject = closeObject;
+                }
+
+            }
+            nav.SetDestination(shieldGameObject.transform.position);
+
+        }
+        
+
     }
 }
--- Kill_The_Streamer/Assets/Scripts/ShieldAi.cs
+++ Kill_The_Streamer/Assets/Scripts/ShieldAi.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class ShieldAi : MonoBehaviour {
 
     // Use this for initialization
-    private GameObject player;
-
     public int shieldHP = 6;//the amount of hits a shield can take
+
+    private bool shieldActive;//tells if the shield is up
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
+        shieldActive = true;
+        //double checking to make sure no one set the shield hp to something lower than 2 at the startup
+        if(shieldHP<=2)
+        {
+            shieldHP = 3;
+        }
 	}
+
+    //property for shieldActive
+    public bool ShieldActive
+    {
+        get { return shieldActive; }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(player.transform.position);
+        CheckingShieldUp();
 	}
+
+
+    public void CheckingShieldUp()
+    {
+        if(shieldHP <= 0)
+        {
+            shieldActive = false;
+            this.gameObject.SetActive(false);
+        }
+    }
 }
