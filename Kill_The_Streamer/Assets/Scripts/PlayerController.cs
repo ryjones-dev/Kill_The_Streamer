95ebe0d78aba032d92c8db4d5e2510881fd5a1ce
Daniel Lowery (RIT Student) <dtl5161@ad.rit.edu>
2017-02-16 13:17:12 -0500

    Fixed dashing and made a quick test scene

--- Kill_The_Streamer/Assets/Scripts/PlayerController.cs
+++ Kill_The_Streamer/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,26 @@ public class PlayerController : MonoBehaviour
 {
 
     //variables
-    public float speed = 0.2f;
+    public float speed = 0.8f;
     public bool dash = false;
-    public float dashSpeed = 28.0f;
+    public float dashSpeed = 25.0f;
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public Vector3 maxVelocity = new Vector3 (5,5,5);
     float dashTime = 0.0f;
 
 
     // Use this for initialization
     void Start()
     {
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //check for input
         Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 tempVelocity = new Vector3(0,0,0);
         cursorPos.y = 0;
         Debug.DrawLine(transform.position, cursorPos, Color.red);
         //check for dash
@@ -34,51 +38,75 @@ public class PlayerController : MonoBehaviour
             }
         }
         //movement
+        
         if (Input.GetKey(KeyCode.W) && dash == false)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                speed = speed * dashSpeed;
-                dash = true;
-                dashTime = Time.time;
 
-            }
-            transform.position = transform.position + new Vector3(speed, 0, 0);
+           tempVelocity += new Vector3(1, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.A) && dash == false)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                speed = speed * dashSpeed;
-                dash = true;
-                dashTime = Time.time;
-            }
-            transform.position = transform.position + new Vector3(0, 0, speed);
+            tempVelocity += new Vector3(0, 0, 1);
         }
 
         if (Input.GetKey(KeyCode.S) && dash == false)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                speed = speed * dashSpeed;
-                dash = true;
-                dashTime = Time.time;
-            }
-            transform.position = transform.position + new Vector3(-speed, 0, 0);
+            tempVelocity += new Vector3(-1, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D) && dash == false)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                speed = speed * dashSpeed;
-                dash = true;
-                dashTime = Time.time;
-            }
-            transform.position = transform.position + new Vector3(0, 0, -speed);
+            tempVelocity += new Vector3(0, 0, -1);
         }
 
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) && dash == false)
+        {
+            speed = speed * dashSpeed;
+            dash = true;
+            dashTime = Time.time;
+        }
+
+        if (tempVelocity != Vector3.zero)
+        {
+            tempVelocity.Normalize();
+        }
+
+        velocity += tempVelocity * speed;
+
+        //cap the speed
+        if(velocity.x >= maxVelocity.x && dash == false)
+        {
+            velocity.x = maxVelocity.x;
+        }
+
+        if (velocity.x <= -maxVelocity.x && dash == false)
+        {
+            velocity.x = -maxVelocity.x;
+        }
+
+        if (velocity.y >= maxVelocity.y && dash == false)
+        {
+            velocity.y = maxVelocity.y;
+        }
+
+        if (velocity.y <= -maxVelocity.y && dash == false)
+        {
+            velocity.y = -maxVelocity.y;
+        }
+
+        if (velocity.z >= maxVelocity.z && dash == false)
+        {
+            velocity.z = maxVelocity.z;
+        }
+
+        if (velocity.z <= -maxVelocity.z && dash == false)
+        {
+            velocity.z = -maxVelocity.z;
+        }
+
+        transform.position += velocity * Time.deltaTime;
+        velocity = velocity * 0.8f;
         //shooting
         if (Input.GetMouseButtonDown(0))
         {
