86d0e37fa72d854b5326d4576e09bbb2448c6e09
MAIN\rc7604 <rc7604@g.rit.edu>
2017-02-24 12:54:23 -0500

    Working on MrDestructoid. Adding a timer system similar to the ghost charging but with exploding.

---
+++ Kill_The_Streamer/Assets/Scripts/AI/AiDestructoid.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AiDestructoid : MonoBehaviour {
+
+    public float destructTimer;//how long till detination
+    private float currentTimer;//the current number on the timer
+
+    private GameObject player; //the player
+
+    public float triggerDistance = 4.0f;//the distance at which the player will be within the range to trigger. 
+    public float damageDistance = 5.0f;//the range at which the player will take damage
+
+    private SpriteRenderer colorChange;//this will hold the sprite of the MrDestructoid to change the color when it will explode
+
+    private bool toExplode;//tells if the Ai is ready to explode
+
+    private NavMeshAgent nav;//the navmeshAgent for the current AI. All AIs need a navMeshAgent to work.
+
+    private float distance;//the distance from AI to player.
+
+    private Color defaultColor;
+
+    // Use this for initialization
+    void Start () {
+        colorChange = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        nav = GetComponent<NavMeshAgent>();//getting the navMesh component of the AI
+
+        toExplode = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+
+    /// <summary>
+    /// Used for seeking out and going to the player.
+    /// Based on NavMesh.
+    /// Changing speed and acceleration can be found in inspector.
+    /// </summary>
+    public void Seek()
+    {
+        nav.SetDestination(player.transform.position);//telling the AI to seek out and go to the player's location
+    }
+
+
+    /// <summary>
+    /// If Destructoid is close enough to the player
+    /// then the destructoid will become invincible and set off the countdown timer
+    /// The destructoid will flash and then explode when it reaches zero
+    /// </summary>
+    private void Explode()
+    {
+        if(toExplode==true)
+        {
+            currentTimer += Time.deltaTime;
+            if(currentTimer%0.2==0)
+            {
+                //To add:
+                //Change color of sprite
+            }
+
+            if(currentTimer >= destructTimer)
+            {
+                //Add in:
+                //Deals damage to player
+                if(distance <=damageDistance)
+                {
+                    //do damage and deactivate self
+                }
+                //deactivates self
+            }
+        }
+    }
+
+}
