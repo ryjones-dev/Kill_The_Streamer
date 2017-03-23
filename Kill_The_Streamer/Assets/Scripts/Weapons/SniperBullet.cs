148850d49a0a02a828c299030393fe3bed4bfe68
Daniel Lowery (RIT Student) <dtl5161@ad.rit.edu>
2017-03-23 13:49:37 -0400

    Added sniper

---
+++ Kill_The_Streamer/Assets/Scripts/Weapons/SniperBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperBullet : MonoBehaviour
+{
+
+    private float timeAlive = 1.5f; // Amount of time until the projectile is destroyed (range)
+    private float speed = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, timeAlive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //should be destroyed if hits wall
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("BooAi"))
+        {
+            EnemyManager.DestroyEnemy(EnemyType.BooEnemy, collision.collider.GetComponent<EnemyData>().m_Index);
+
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            //Debug.Log("Collided with obstacle");
+            Destroy(gameObject);
+        }
+    }
+}
