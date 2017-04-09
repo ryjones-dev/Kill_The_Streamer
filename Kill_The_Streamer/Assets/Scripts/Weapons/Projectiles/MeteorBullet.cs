b93ee9b2314eb09abada62775020a2bef52b611b
Ryan Jones <rpj5582@rit.edu>
2017-04-09 17:55:13 -0400

    Created meteor gun and bullet

---
+++ Kill_The_Streamer/Assets/Scripts/Weapons/Projectiles/MeteorBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorBullet : MonoBehaviour
+{
+    private void Update()
+    {
+        if(transform.position.y <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localScale *= 0.97f;
+        if (transform.localScale.magnitude < 1.0f)
+        {
+            transform.localScale = transform.localScale.normalized;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            AIBase ai = other.GetComponent<AIBase>();
+            ai.TakeDamage();
+        }
+    }
+}
