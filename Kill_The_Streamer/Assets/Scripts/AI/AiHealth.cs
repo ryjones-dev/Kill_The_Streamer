cc10e73c434984da23b4c93c4efe3480f6f9a357
Ro <rc7604@g.rit.edu>
2017-04-24 18:11:00 -0400

    Working on health pack when certain enemies die.

---
+++ Kill_The_Streamer/Assets/Scripts/AI/AiHealth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiHealth : AIBase {
+
+    // Use this for initialization
+    //all AI needs using UnityEngine.AI;
+    private NavMeshAgent nav;//the navmeshAgent for the current AI. All AIs need a navMeshAgent to work.
+    public float speed = 5.0f;
+
+    public GameObject healthPrefab;
+    void Start () {
+        nav = GetComponent<NavMeshAgent>();//getting the navMesh component of the AI
+    }
+
+    // Update is called once per frame
+    /*
+	public override void Update () {
+        base.Start();
+	}
+    */
+
+    public override void AILoop()
+    {
+
+    }
+
+
+    public override void DealDamage()
+    {
+        Player.s_Player.TakeDamage(1000, name, true);
+    }
+
+
+    public override void UpdateSpeed()
+    {
+        nav.speed = speed;
+    }
+
+    public override void TakeDamage()
+    {
+        health--;
+        if (health <= 0)
+        {
+            Instantiate(healthPrefab, this.transform.position,Quaternion.identity);
+            EnemyManager.DestroyEnemy(aiType, index);
+            
+
+        }
+    }
+
+
+
+}
---
+++ Kill_The_Streamer/Assets/Scripts/AI/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int healthIncrease = 100;
+
+	// Use this for initialization
+	void Start () {
+		//Player.s_Player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag=="Player")
+        {
+            Player.s_Player.m_health += healthIncrease;
+        }
+    }
+}
